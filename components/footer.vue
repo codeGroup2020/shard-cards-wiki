0733f849b74983802895c1b5d581eac19890b1a5
Nathaniel <nathaniel@watsonrentals.net>
2025-05-05 17:47:57 -0500

    Add discord link to footer

--- components/footer.vue
+++ components/footer.vue
@@ -3,6 +3,12 @@
     <main class="bg-slate-700">
         <footer class="bg-slate-600 text-white min-h-[10rem] p-5 flex flex-col justify-center">
             <p class="text-center text-xs font-sans">
+                Use the link below to join the wiki's discord server if you want to be notified when the wiki is updated or to contribute to the wiki.
+            </p>
+            <a href="https://discord.gg/W2CpkSJGBp" class="text-center text-sm font-sans underline text-blue-500">
+                https://discord.gg/W2CpkSJGBp
+            </a>
+            <p class="text-center text-xs font-sans">
                 ShardCards.gg is an unofficial, fan‑operated website and is not affiliated with Bad Guitar Studio, NetEase Games, or any of their affiliates. All game names, characters, artwork, logos and related trademarks are the property of their respective owners. Content on this site is provided for informational purposes only; while we strive for accuracy, we make no warranties as to its completeness or reliability. All views and opinions expressed here are those of the individual authors and do not reflect the official positions of Bad Guitar Studio, NetEase Games, or their affiliates.
             </p>
         </footer>
